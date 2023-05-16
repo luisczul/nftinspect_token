e57ed531cc75996f9a71faf9b9856655af881399
Diego <luisczul@gmail.com>
2023-05-15 21:56:05 -0400

    Added distributiong for testing

--- contracts/Inspect.sol
+++ contracts/Inspect.sol
@@ -11,7 +11,33 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 /// @custom:security-contact tech@nftinspect.xyz
 contract Inspect is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, ERC20Permit, ERC20Votes {
     constructor() ERC20("Inspect", "INSP") ERC20Permit("Inspect") {
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
+
+        uint256 initialSupply = 1000000000 * 10 ** uint256(decimals());
+
+        address public_sale = 0x853dcC2dDAf853383D909480f4a5B2B00811baEB; //PUBLIC SALE 12% 
+        address strategicSale = 0x69d61993c1D442fd4b708874637EDd6d481DD842; // STRATEGIC SALE 7%
+        address ecosystemGrowth = 0x9A77539bC0a6c297715A521361133138f82cAe11; //ECOSYSTEM GROWTH 10%
+        address liquidity = 0xD15F0F1EB2201A48d92d11229e4A046C60A24005; //LIQUIDITY 20%
+        address advisors = 0x95CA6fd0477adCd9E42446c19302Cab533Ab891a; //ADVISORS 5%
+        address communityFoundation = 0x60e872A640FC18DC24c161dd88C530368c28aF67; //COMMUNITY_FOUNDATION 28%
+        address coreContributors = 0xA94BfDA106554fb8AD107179a08d704e377B0574; //CORE CONTRIBUTORS 18%
+        
+        uint256 public_sale_amount = initialSupply * 12 / 100;
+        uint256 strategicSale_amount = initialSupply * 7 / 100;
+        uint256 ecosystemGrowth_amount = initialSupply * 10 / 100;
+        uint256 liquidity_amount = initialSupply * 20 / 100;
+        uint256 advisors_amount = initialSupply * 5 / 100;
+        uint256 communityFoundation_amount = initialSupply * 28 / 100;
+        uint256 coreContributors_amount = initialSupply * 18 / 100;
+
+        _mint(public_sale, public_sale_amount);
+        _mint(strategicSale, strategicSale_amount);
+        _mint(ecosystemGrowth, ecosystemGrowth_amount);
+        _mint(liquidity, liquidity_amount);
+        _mint(advisors, advisors_amount);
+        _mint(communityFoundation, communityFoundation_amount);
+        _mint(coreContributors, coreContributors_amount);
+
     }
 
     function snapshot() public onlyOwner {
@@ -46,5 +72,9 @@ contract Inspect is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, ERC20Permit, E
         override(ERC20, ERC20Votes)
     {
         super._burn(account, amount);
+    }
+
+    function distributeTokens(uint256 amount) private {
+        
     }
 }
